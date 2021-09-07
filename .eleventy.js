ec881cb75423cf459ba0d7b7dea83e828599ddd1
BiigFish <83352674+BiigFish@users.noreply.github.com>
2021-09-07 10:36:55 -0400

    passthrough admin folder

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,10 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/assets/style.css");
     eleventyConfig.addPassthroughCopy("src/assets/favicon.ico");
 
+    eleventyConfig.addPassthroughCopy({
+      "./src/admin/config.yml": "./admin/config.yml",
+    });
+
   
   
     return {
